568a5764f197739b18355b43cd0c037986d46e79
gkirion <gkirion@gmail.com>
2022-08-06 23:38:24 +0300

    Replaced place repository with place service in sensor reading service

--- src/main/java/com/george/service/SensorReadingService.java
+++ src/main/java/com/george/service/SensorReadingService.java
@@ -4,7 +4,6 @@ import com.george.exception.PlaceNotFoundException;
 import com.george.model.LandStatus;
 import com.george.model.Place;
 import com.george.model.SensorReading;
-import com.george.repository.PlaceRepository;
 import com.george.repository.SensorReadingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,10 +17,10 @@ public class SensorReadingService {
     private SensorReadingRepository sensorReadingRepository;
 
     @Autowired
-    private PlaceRepository placeRepository;
+    private PlaceService placeService;
 
     public SensorReading insert(LandStatus landStatus) throws PlaceNotFoundException {
-        Place place = placeRepository.findByName(landStatus.getPlace()).orElseThrow(() -> new PlaceNotFoundException("place " + landStatus.getPlace() + " does not exist"));
+        Place place = placeService.findByName(landStatus.getPlace());
         SensorReading sensorReading = new SensorReading();
         sensorReading.setPlace(place);
         sensorReading.setTimestamp(new Date());
